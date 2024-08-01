f9e763864403842cc247eaa1656bf6ea6e37fe96
Rohit Hazra <rohithzr@live.com>
2024-08-01 01:56:46 -0700

    stream false in test fix and lint

--- examples/knowledgebase.py
+++ examples/knowledgebase.py
@@ -11,18 +11,20 @@ client = PropulsionAI(
 
 def sync_main() -> None:
     knowledgebase = client.knowledgebase.create(name="weather", description="A knowledgebase for weather", tags="uppto")
-    print(f"Knowledgebase created with id: ", knowledgebase.id)
+    print(f"Knowledgebase created with id: ", knowledgebase.code)
     # read ./12025248.pdf and convert it to bytes
     file_path = Path("examples/12025248.pdf")
-    if knowledgebase.id:
+    if knowledgebase.code:
         with open(file_path, "rb") as f:
             file_bytes = f.read()
-            upload_file_response = client.knowledgebase.file.upload(knowledgebase_id=knowledgebase.id, file=file_bytes)
+            upload_file_response = client.knowledgebase.file.upload(
+                knowledgebase_code=knowledgebase.code, file=file_bytes
+            )
             print(f"File uploaded with id: ", upload_file_response.id)
         if upload_file_response.id:
             # delete the file
             delete_file_response = client.knowledgebase.file.delete(
-                knowledgebase_id=knowledgebase.id, file_id=upload_file_response.id
+                knowledgebase_code=knowledgebase.code, file_id=upload_file_response.id
             )
             print(f"File deleted with id: ", delete_file_response.id)
 
